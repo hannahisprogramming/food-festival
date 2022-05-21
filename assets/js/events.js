e549c9b9e38212539fd53c2e8896934d04e25be4
Hannah Bush <heb.rutgers@gmail.com>
2022-05-20 20:56:24 -0400

    small errors fixed in how helper.js was linked; build reran

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,6 +1,6 @@
 require("bootstrap");
 const createEl = require("./domMethods");
-const createLoremIpsum = require("./helpers");
+const createLoremIpsum = require("./helper");
 
 $(document).ready( function() {
   const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
